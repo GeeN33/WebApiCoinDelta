e232d224340274a1c3b2c34da36cf2edd680bffd
Evgeniy <eugtrader@gmail.com>
2024-07-14 14:18:33 +0300

    Добавьте файлы проекта.

---
+++ WebApiCoinDelta/Program.cs
@@ -0,0 +1,32 @@
+using Npgsql;
+using System.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IDbConnection>((s) =>
+{
+    IDbConnection conn = new NpgsqlConnection(builder.Configuration.GetConnectionString("DefaultConnection"));
+    conn.Open();
+    return conn;
+});
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.MapControllers();
+
+app.MapGet("/", () => "Hi GeeN!");
+
+app.Run();
+
